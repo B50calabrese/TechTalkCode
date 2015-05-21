018042136f319b6f2ce3cdd0d54c03813d79d085
Antonio Calabrese <acalabrese@google.com>
2015-05-21 14:13:46 -0400

    The basic engine of the game is there, now I just have to make the maze look okay and it should be all set.

--- CursesLibTutorial/tutorial_game.c
+++ CursesLibTutorial/tutorial_game.c
@@ -28,29 +28,22 @@ int main(int argc, char *argv[]) {
   int j;
 
   char MAZE[MAZE_HEIGHT][MAZE_WIDTH] = {
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'},
-  	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'}
+  	{ WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR},
+  	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, LADDER_CHAR, WALL_CHAR},
+  	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+  	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+  	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, BLANK_CHAR, WALL_CHAR},
+	{ WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR, WALL_CHAR}
   };
-
-  // First create a blank maze
-  /*for (i = 0 ; i < MAZE_HEIGHT ; i++) {
-  	for (j = 0 ; j < MAZE_WIDTH ; j++) {
-  		MAZE[i][j] = BLANK_CHAR;
-  	}
-  }*/
 
   // Called to initialize the screen
   initscr();
@@ -70,10 +63,85 @@ int main(int argc, char *argv[]) {
   	}
   }
 
+  // Add the player
+  move(player_y, player_x);
+  addch(PLAYER_CHAR);
+
   // Now display the maze
   refresh();
 
-  sleep(1);
+  // Main game loop
+  while (done == 0) {
+    int charPress;
+    int oldX;
+    int oldY;
+    int shouldMove;
+
+    shouldMove = 0;
+    oldX = player_x;
+    oldY = player_y;
+
+    // Get the player's move
+    charPress = getch();
+
+    // Since we surrounded the maze with walls, we only need to check that the
+    // the player didn't move onto a wall
+    if (charPress == 'w') {
+      if (MAZE[player_y - 1][player_x] != WALL_CHAR) {
+        // If they moved up not into a wall
+
+	// We should set move to true
+        shouldMove = 1;
+        
+        player_y = player_y - 1;
+     }
+    } else if (charPress == 's') {
+      if (MAZE[player_y + 1][player_x] != WALL_CHAR) {
+        // If they moved down not into a wall
+
+        // We should set move to true
+        shouldMove = 1;
+
+        player_y = player_y + 1;
+      }
+    } else if (charPress == 'a') {
+      if (MAZE[player_y][player_x - 1] != WALL_CHAR) {
+        // If they moved right not into a wall
+
+	// We should set move to true
+        shouldMove = 1;
+        
+        player_x = player_x - 1;
+      }
+    } else if (charPress == 'd') {
+      if (MAZE[player_y][player_x + 1] != WALL_CHAR) {
+        // If they moved left not into a wall
+
+        // We should set move to true
+        shouldMove = 1;
+
+        player_x = player_x + 1;
+      }
+    }
+
+    // If the player moved, we need to update the screen
+    if (shouldMove) {
+      // We need to reset the previous tile
+      move(oldY, oldX);
+      addch(MAZE[oldY][oldX]);
+
+      // Now we will draw the player
+      move(player_y, player_x);
+      addch(PLAYER_CHAR);
+
+      refresh();   
+    }
+
+    // If the player made it to the ladder
+    if (MAZE[player_y][player_x] == LADDER_CHAR) {
+      done = 1;
+    }  
+  }
 
   // Get rid of the curses window and return to the normal terminal's behavior
   endwin();
